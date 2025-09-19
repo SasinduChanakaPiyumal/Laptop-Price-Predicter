a9022c5ca51de8a82a21a91ff3862139fa5333c7
Artemis Git Integration <artemis@turintech.ai>
2025-09-19 11:37:44 +0000

    feat: add pre-commit hooks with code formatting and linting tools
    Add comprehensive pre-commit configuration with black, isort, flake8, and whitespace fixers. Configure black with line-length 88 and Python 3.11 target, isort with black profile compatibility, and flake8 with max-line-length 88 ignoring E203, W503. Apply automatic formatting fixes to laptop price model code, consolidate imports, improve code structure, and remove Jupyter notebook cell markers with spacing cleanup.

--- Laptop Price model(1).py
+++ Laptop Price model(1).py
@@ -1,364 +1,195 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+import pickle
 
-
-import pandas as pd
 import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Lasso, LinearRegression
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.tree import DecisionTreeRegressor
 
-
-# In[2]:
-
-
-dataset = pd.read_csv("laptop_price.csv",encoding = 'latin-1')
-
-
-# In[3]:
-
+# Load and explore dataset
+dataset = pd.read_csv("laptop_price.csv", encoding="latin-1")
 
 dataset.head()
-
-
-# In[4]:
-
-
 dataset.shape
-
-
-# In[5]:
-
-
 dataset.describe()
-
-
-# In[6]:
-
-
 dataset.info()
-
-
-# In[7]:
-
-
 dataset.isnull().sum()
 
-
-# In[8]:
-
-
-dataset['Ram']=dataset['Ram'].str.replace('GB','').astype('int32')
-
-
-# In[9]:
-
-
+# Data preprocessing
+dataset["Ram"] = dataset["Ram"].str.replace("GB", "").astype("int32")
 dataset.head()
 
-
-# In[10]:
-
-
-dataset['Weight']=dataset['Weight'].str.replace('kg','').astype('float64')
-
-
-# In[11]:
-
-
+dataset["Weight"] = dataset["Weight"].str.replace("kg", "").astype("float64")
 dataset.head(2)
 
-
-# In[12]:
-
-
-non_numeric_columns = dataset.select_dtypes(exclude=['number']).columns
+non_numeric_columns = dataset.select_dtypes(exclude=["number"]).columns
 print(non_numeric_columns)
 
-
-# In[13]:
-
-
 numeric_dataset = dataset.drop(columns=non_numeric_columns)
-correlation = numeric_dataset.corr()['Price_euros']
-
-
-# In[14]:
-
-
+correlation = numeric_dataset.corr()["Price_euros"]
 correlation
 
-
-# In[15]:
-
-
-dataset['Company'].value_counts()
-
-
-# In[16]:
+dataset["Company"].value_counts()
 
 
 def add_company(inpt):
-    if inpt == 'Samsung'or inpt == 'Razer' or inpt == 'Mediacom' or inpt == 'Microsoft' or inpt == 'Xiaomi' or inpt == 'Vero' or inpt == 'Chuwi' or inpt == 'Google' or inpt == 'Fujitsu' or inpt == 'LG' or inpt == 'Huawei':
-        return 'Other'
+    other_companies = [
+        "Samsung",
+        "Razer",
+        "Mediacom",
+        "Microsoft",
+        "Xiaomi",
+        "Vero",
+        "Chuwi",
+        "Google",
+        "Fujitsu",
+        "LG",
+        "Huawei",
+    ]
+    if inpt in other_companies:
+        return "Other"
     else:
         return inpt
-dataset['Company'] = dataset['Company'].apply(add_company)
 
 
-# In[17]:
+dataset["Company"] = dataset["Company"].apply(add_company)
+dataset["Company"].value_counts()
 
+len(dataset["Product"].value_counts())
+dataset["TypeName"].value_counts()
+dataset["ScreenResolution"].value_counts()
 
-dataset['Company'].value_counts()
+# Feature engineering
+dataset["Touchscreen"] = dataset["ScreenResolution"].apply(
+    lambda x: 1 if "Touchscreen" in x else 0
+)
+dataset["IPS"] = dataset["ScreenResolution"].apply(
+    lambda x: 1 if "IPS" in x else 0
+)
 
-
-# In[18]:
-
-
-len(dataset['Product'].value_counts())
-
-
-# In[19]:
-
-
-dataset['TypeName'].value_counts()
-
-
-# In[20]:
-
-
-dataset['ScreenResolution'].value_counts()
-
-
-# In[21]:
-
-
-dataset['Touchscreen'] = dataset['ScreenResolution'].apply(lambda x:1 if 'Touchscreen' in x else 0)
-dataset['IPS'] = dataset['ScreenResolution'].apply(lambda x:1 if 'IPS' in x else 0)
-
-
-# In[22]:
-
-
-dataset['Cpu'].value_counts()
-
-
-# In[23]:
-
-
-dataset['Cpu_name']= dataset['Cpu'].apply(lambda x:" ".join(x.split()[0:3]))
-
-
-# In[24]:
-
-
-dataset['Cpu_name'].value_counts()
-
-
-# In[25]:
+dataset["Cpu"].value_counts()
+dataset["Cpu_name"] = dataset["Cpu"].apply(lambda x: " ".join(x.split()[0:3]))
+dataset["Cpu_name"].value_counts()
 
 
 def set_processor(name):
-    if name == 'Intel Core i7' or name == 'Intel Core i5' or name == 'Intel Core i3':
+    intel_processors = ["Intel Core i7", "Intel Core i5", "Intel Core i3"]
+    if name in intel_processors:
         return name
     else:
-        if name.split()[0] == 'AMD':
-            return 'AMD'
+        if name.split()[0] == "AMD":
+            return "AMD"
         else:
-            return 'Other'
-dataset['Cpu_name'] = dataset['Cpu_name'].apply(set_processor)
+            return "Other"
 
 
-# In[26]:
+dataset["Cpu_name"] = dataset["Cpu_name"].apply(set_processor)
+dataset["Cpu_name"].value_counts()
 
+dataset["Gpu_name"] = dataset["Gpu"].apply(lambda x: " ".join(x.split()[0:1]))
+dataset["Gpu_name"].value_counts()
 
-dataset['Cpu_name'].value_counts()
-
-
-# In[27]:
-
-
-dataset['Gpu_name']= dataset['Gpu'].apply(lambda x:" ".join(x.split()[0:1]))
-
-
-# In[30]:
-
-
-dataset['Gpu_name'].value_counts()
-
-
-# In[29]:
-
-
-dataset = dataset[dataset['Gpu_name'] != 'ARM']
-
-
-# In[32]:
-
-
+# Filter out ARM GPUs
+dataset = dataset[dataset["Gpu_name"] != "ARM"]
 dataset.head(2)
 
-
-# In[35]:
-
-
-dataset['OpSys'].value_counts()
-
-
-# In[34]:
+dataset["OpSys"].value_counts()
 
 
 def set_os(inpt):
-    if inpt == 'Windows 10' or inpt == 'Windows 7' or inpt == 'Windows 10 S':
-        return 'Windows'
-    elif inpt == 'macOS' or inpt == 'Mac OS X':
-        return 'Mac'
-    elif inpt == 'Linux':
+    windows_versions = ["Windows 10", "Windows 7", "Windows 10 S"]
+    mac_versions = ["macOS", "Mac OS X"]
+    
+    if inpt in windows_versions:
+        return "Windows"
+    elif inpt in mac_versions:
+        return "Mac"
+    elif inpt == "Linux":
         return inpt
     else:
-        return 'Other'
-dataset['OpSys']= dataset['OpSys'].apply(set_os)
+        return "Other"
 
 
-# In[37]:
+dataset["OpSys"] = dataset["OpSys"].apply(set_os)
 
-
-dataset=dataset.drop(columns=['laptop_ID','Inches','Product','ScreenResolution','Cpu','Gpu'])
-
-
-# In[38]:
-
-
+# Drop unnecessary columns
+dataset = dataset.drop(
+    columns=["laptop_ID", "Inches", "Product", "ScreenResolution", "Cpu", "Gpu"]
+)
 dataset.head()
 
-
-# In[39]:
-
-
+# Create dummy variables
 dataset = pd.get_dummies(dataset)
-
-
-# In[40]:
-
-
 dataset.head()
 
+# Prepare features and target
+x = dataset.drop("Price_euros", axis=1)
+y = dataset["Price_euros"]
 
-# In[41]:
-
-
-x = dataset.drop('Price_euros',axis=1)
-y = dataset['Price_euros']
-
-
-# In[50]:
-
-
-pip install scikit-learn
-
-
-# In[51]:
-
-
-from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.25)
-
-
-# In[53]:
-
-
-x_train.shape,x_test.shape
-
-
-# In[54]:
+# Split the data
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+x_train.shape, x_test.shape
 
 
 def model_acc(model):
-    model.fit(x_train,y_train)
+    """Calculate and print model accuracy."""
+    model.fit(x_train, y_train)
     acc = model.score(x_test, y_test)
-    print(str(model)+'-->'+str(acc))
+    print(str(model) + "-->" + str(acc))
 
 
-# In[57]:
-
-
-from sklearn.linear_model import LinearRegression
+# Train different models
 lr = LinearRegression()
 model_acc(lr)
 
-from sklearn.linear_model import Lasso
 lasso = Lasso()
 model_acc(lasso)
 
-from sklearn.tree import DecisionTreeRegressor
 dt = DecisionTreeRegressor()
 model_acc(dt)
 
-from sklearn.ensemble import RandomForestRegressor
 rf = RandomForestRegressor()
 model_acc(rf)
 
+# Hyperparameter tuning
+parameters = {
+    "n_estimators": [10, 50, 100],
+    "criterion": ["squared_error", "absolute_error", "poisson"],
+}
 
-# In[58]:
-
-
-from sklearn.model_selection import GridSearchCV
-
-parameters = {'n_estimators':[10,50,100],'criterion':['squared_error','absolute_error','poisson']}
-
-grid_obj = GridSearchCV(estimator = rf ,param_grid = parameters)
-
-grid_fit = grid_obj.fit(x_train,y_train)
-
+grid_obj = GridSearchCV(estimator=rf, param_grid=parameters)
+grid_fit = grid_obj.fit(x_train, y_train)
 best_model = grid_fit.best_estimator_
 best_model
 
-
-# In[59]:
-
-
-best_model.score(x_test,y_test)
-
-
-# In[60]:
-
-
+best_model.score(x_test, y_test)
 x_train.columns
 
+# Save the model
+with open("predictor.pickle", "wb") as file:
+    pickle.dump(best_model, file)
 
-# In[68]:
+# Make predictions
+sample_features = [
+    [8, 1.4, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1]
+]
+best_model.predict(sample_features)
 
+sample_features_2 = [
+    [8, 0.9, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1]
+]
+best_model.predict(sample_features_2)
 
-import pickle
-with open('predictor.pickle','wb') as file:
-    pickle.dump(best_model,file)
+sample_features_3 = [
+    [8, 1.2, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1]
+]
+best_model.predict(sample_features_3)
 
-
-# In[66]:
-
-
-best_model.predict([[8,1.4,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1]])
-
-
-# In[69]:
-
-
-best_model.predict([[8,0.9,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1]])
-
-
-# In[70]:
-
-
-best_model.predict([[8,1.2,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1]])
-
-
-# In[71]:
-
-
-best_model.predict([[8,0.9,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1]])
-
-
-# In[ ]:
-
-
-
-
+sample_features_4 = [
+    [8, 0.9, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1]
+]
+best_model.predict(sample_features_4)
